9c1b3c067216bd351d722fe3fde22cd05177cca2
Kacper Wach <wach.kacper8@gmail.com>
2024-10-26 12:39:35 +0200

    Dodano zadanie 4

---
+++ POC sem7/lab2/bialeKolko.m
@@ -0,0 +1,23 @@
+function [img] = bialeKolko(radius)
+   
+    % Parametry obrazu
+img_size = 512;
+%radius = img_size / 4; % promień koła
+
+% Tworzenie siatki współrzędnych
+[x, y] = meshgrid(1:img_size, 1:img_size);
+
+% Obliczanie odległości od środka
+center = img_size / 2;
+distance_from_center = sqrt((x - center).^2 + (y - center).^2);
+
+% Tworzenie obrazu z jedynkami i zerami w kształcie koła
+img = zeros(img_size);
+img(distance_from_center <= radius) = 1;
+
+% Wyświetlanie obrazu
+%imshow(img);
+
+
+
+end
